23bbf9d6273747a421668562df94028d15fcc430
Cloud User <ec2-user@ip-172-31-38-0.us-west-2.compute.internal>
2022-05-27 03:37:22 +0000

    Age Telling script

---
+++ AdeA.sh
@@ -0,0 +1,15 @@
+echo "Enter your name"
+read name
+
+echo "$name"
+echo " enter your age"
+read age
+a=5
+sum=$(($age + 5))
+
+echo "HellO $name"
+echo "In five years time you will be"
+echo "$sum" yrs old
+echo "Thank you!"
+
+
